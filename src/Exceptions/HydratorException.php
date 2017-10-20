8cdb06875c3ee4ae3b82f44cf513b430dc6e7a59
XAKEPEHOK <icq-475204@ya.ru>
2017-10-20 17:24:29 +0300

    Класс исключения HydratorException

---
+++ src/Exceptions/HydratorException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 20.10.2017
+ * Time: 17:20
+ */
+
+
+namespace DjinORM\Djin\Exceptions;
+
+
+class HydratorException extends \Exception
+{
+
+}
