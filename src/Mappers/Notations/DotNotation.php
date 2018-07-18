c910149736a6c5fe63c58fd8b9b7f5b8fbba5284
XAKEPEHOK <icq-475204@ya.ru>
2018-07-18 14:05:33 +0300

    DotNotation extends CustomCharNotation

--- src/Mappers/Notations/DotNotation.php
+++ src/Mappers/Notations/DotNotation.php
@@ -8,50 +8,12 @@
 namespace DjinORM\Djin\Mappers\Notations;
 
 
-use Adbar\Dot;
-use RecursiveArrayIterator;
-use RecursiveIteratorIterator;
-
-class DotNotation implements NotationInterface
+class DotNotation extends CustomCharNotation
 {
 
-    /**
-     * @param $array
-     * @return mixed
-     */
-    public function encode(array $array)
+    public function __construct()
     {
-        $iterator = new RecursiveIteratorIterator(
-            new RecursiveArrayIterator($array)
-        );
-
-        $encoded = [];
-        foreach ($iterator as $value) {
-            $keys = array();
-            foreach (range(0, $iterator->getDepth()) as $depth) {
-                $keys[] = $iterator->getSubIterator($depth)->key();
-            }
-            $dotKey = join('.', $keys);
-            $encoded[$dotKey] = $value;
-        }
-        return $encoded;
+        parent::__construct('.');
     }
 
-    /**
-     * @param $encodedValue
-     * @return array
-     */
-    public function decode($encodedValue): array
-    {
-        $decoded = new Dot();
-        foreach ($encodedValue as $dotKey => $value) {
-            $decoded->set($dotKey, $value);
-        }
-        return $decoded->all();
-    }
-
-    public function isDecodeFirst(): bool
-    {
-        return true;
-    }
 }
