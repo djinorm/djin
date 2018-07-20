6d95194d4b10efcadd4d59bdd4868cec092e6236
XAKEPEHOK <icq-475204@ya.ru>
2018-07-20 17:11:46 +0300

    SubclassMapper allowNull

--- src/Mappers/SubclassMapper.php
+++ src/Mappers/SubclassMapper.php
@@ -19,36 +19,52 @@ class SubclassMapper extends AbstractMapper implements NestedMapperInterface
      */
     protected $nestedMapper;
 
-    public function __construct(string $modelProperty, string $dbAlias, MappersHandlerInterface $nestedMapper)
+    public function __construct(string $modelProperty, string $dbAlias, MappersHandlerInterface $nestedMapper, bool $allowNull = false)
     {
         $this->modelProperty = $modelProperty;
         $this->dbAlias = $dbAlias;
         $this->nestedMapper = $nestedMapper;
+        $this->allowNull = $allowNull;
     }
 
     /**
      * @param array $data
      * @param object $object
      * @return mixed
+     * @throws \DjinORM\Djin\Exceptions\HydratorException
      * @throws \ReflectionException
      */
     public function hydrate(array $data, $object)
     {
+        if (!isset($data[$this->getDbAlias()])) {
+            if ($this->isAllowNull()) {
+                RepoHelper::setProperty($object, $this->getModelProperty(), null);
+                return null;
+            }
+            throw $this->nullHydratorException($this->nestedMapper->getModelClassName(), $object);
+        }
+
         $data = $data[$this->getDbAlias()];
         $subObject = $this->nestedMapper->hydrate($data);
-        RepoHelper::setProperty($object, $this->modelProperty, $subObject);
+        RepoHelper::setProperty($object, $this->getModelProperty(), $subObject);
         return $subObject;
     }
 
     /**
      * @param $object
      * @return array
+     * @throws \DjinORM\Djin\Exceptions\ExtractorException
      * @throws \ReflectionException
      */
     public function extract($object): array
     {
         $subObject = RepoHelper::getProperty($object, $this->modelProperty);
+
         $data = $this->nestedMapper->extract($subObject);
+        if ($data === null && $this->isAllowNull() == false) {
+            throw $this->nullExtractorException($this->nestedMapper->getModelClassName(), $object);
+        }
+
         return [
             $this->getDbAlias() => $data,
         ];
