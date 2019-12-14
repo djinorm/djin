169aee658599959beb74a7cf97399c98fcb4088b
XAKEPEHOK <icq-475204@ya.ru>
2019-12-14 22:35:58 +0300

    DummyLockerTest

--- src/Locker/DummyLocker.php
+++ src/Locker/DummyLocker.php
@@ -4,42 +4,100 @@
 namespace DjinORM\Djin\Locker;
 
 
+use DjinORM\Djin\Exceptions\InvalidArgumentException;
 use DjinORM\Djin\Model\Link;
 use DjinORM\Djin\Model\ModelInterface;
 
 class DummyLocker implements LockerInterface
 {
 
+    /** @var ModelInterface[][] */
+    private $locks = [];
+
     public function lock(ModelInterface $model, ModelInterface $locker, int $timeout = null): bool
     {
+        $locked = $this->isLockedFor($model, $locker);
+        if ($locked) {
+            return false;
+        }
+
+        $name = $model::getModelName();
+        $id = (string) $model->getId();
+
+        $this->locks[$name][$id] = $locker;
         return true;
     }
 
     public function unlock(ModelInterface $model, ModelInterface $locker): bool
     {
+        $locked = $this->isLockedFor($model, $locker);
+        if ($locked) {
+            return false;
+        }
+
+        $name = $model::getModelName();
+        $id = (string) $model->getId();
+
+        unset($this->locks[$name][$id]);
         return true;
     }
 
-    public function passLock(ModelInterface $model, ModelInterface $currentLocker, ModelInterface $nextLocker, int $timeout): bool
+    public function passLock(ModelInterface $model, ModelInterface $currentLocker, ModelInterface $nextLocker, int $timeout = null): bool
     {
+        $locked = $this->isLockedFor($model, $currentLocker);
+        if ($locked) {
+            return false;
+        }
+
+        $name = $model::getModelName();
+        $id = (string) $model->getId();
+
+        $this->locks[$name][$id] = $nextLocker;
         return true;
     }
 
     public function isLockedFor(ModelInterface $model, ?ModelInterface $locker): bool
     {
-        return false;
-    }
+        $name = $model::getModelName();
+        $id = (string) $model->getId();
 
-    public function getDefaultTimeout(): int
-    {
-        return 0;
+        if (!isset($this->locks[$name][$id])) {
+            return false;
+        }
+
+        $currentLocker = $this->locks[$name][$id];
+        return $currentLocker !== $locker;
     }
 
     /**
      * @inheritDoc
+     * @throws InvalidArgumentException
      */
     public function getLocker($modelOrLink): ?Link
     {
-        return null;
+        if ($modelOrLink instanceof ModelInterface) {
+            $name = $modelOrLink::getModelName();
+            $id = (string) $modelOrLink->getId();
+        }
+
+        if ($modelOrLink instanceof Link) {
+            $name = $modelOrLink->getModelName();
+            $id = (string) $modelOrLink->getId();
+        }
+
+        if (isset($name) && isset($id)) {
+            if (isset($this->locks[$name][$id])) {
+                return Link::to($this->locks[$name][$id]);
+            }
+            return null;
+        }
+
+        throw new InvalidArgumentException("Locker can be resolved by model or link");
     }
+
+    public function getDefaultTimeout(): int
+    {
+        return 10;
+    }
+
 }
